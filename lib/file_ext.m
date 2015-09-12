1b3d141a9ce5659687e24f678222563d725785dc
iavr <iavr@users.noreply.github.com>
2015-09-12 12:59:29 +0300

    camera

--- lib/file_ext.m
+++
@@ -1,9 +0,0 @@
-function [x, n] = file_ext(n)
-
-% x: extension
-% n: file name without extension (output)
-% n: file name with extension (input)
-
-i = regexp(n, '\.[^\.]*$');
-x = n(i+1:end);
-if ~isempty(i), n = n(1:i-1); end
